c1a9e14776331b7c05fe2c013a65265fad5fa202
ref <56499429+referefref@users.noreply.github.com>
2024-02-26 12:08:52 +0800

    Bug fix
    Updated logging level to DEBUG
    Resolved call in generate_response to dump() utf-8 conversion (fixed upstream)

--- honeyprint.py
+++ honeyprint.py
@@ -1,21 +1,3 @@
-# Copyright (C) 2013  Lukas Rist <glaslos@gmail.com>
-# Copyright (C) 2024  James Brine <james@jamesbrine.com.au>
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
 #!/usr/bin/python3
 
 import logging
@@ -29,42 +11,57 @@ handler.setFormatter(colorlog.ColoredFormatter(
 
 logger = colorlog.getLogger(__name__)
 logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 class PrintServer(object):
 
     def __init__(self):
         pass
 
+    def log_request_attributes(self, request):
+        if hasattr(request, 'operation'):
+            for attribute, value in request.operation.items():
+                logger.debug(f'Operation attribute: {attribute}, value: {value}')
+        if hasattr(request, 'job'):
+            for attribute, value in request.job.items():
+                logger.debug(f'Job attribute: {attribute}, value: {value}')
+        if hasattr(request, 'printer'):
+            for attribute, value in request.printer.items():
+                logger.debug(f'Printer attribute: {attribute}, value: {value}')
+
     def handle(self, sock, address):
-        logger.info(f'Connection from {address}')
+        logger.debug(f'Handling connection from {address}')
+    
+        # Receiving data
         data = sock.recv(8192)
         logger.debug(f'Received data: {repr(data)}')
-
+        
+        # Processing request
         try:
             body = data.split(b'\r\n\r\n', 1)[1]
+            logger.debug('Extracted request body')
         except IndexError:
             body = data
+            logger.warning('No separate request body found, using entire data as body')
+            
+        # Parsing request
         request = pkipplib.IPPRequest(body)
         request.parse()
         logger.info(f'Received request: {request}')
         
-        if hasattr(request, 'operation'):
-            for attribute, value in request.operation.items():
-                logger.debug(f'Request operation attribute: {attribute}, value: {value}')
-        if hasattr(request, 'job'):
-            for attribute, value in request.job.items():
-                logger.debug(f'Request job attribute: {attribute}, value: {value}')
-        if hasattr(request, 'printer'):
-            for attribute, value in request.printer.items():
-                logger.debug(f'Request printer attribute: {attribute}, value: {value}')
-
-        request = pkipplib.IPPRequest(operation_id=pkipplib.CUPS_GET_DEFAULT)
-        request.operation["attributes-charset"] = ("charset", "utf-8")
-        request.operation["attributes-natural-language"] = ("naturalLanguage", "en-us")
-        response_data = request.dump().encode('utf-8')
+        # Log detailed request attributes if needed
+        self.log_request_attributes(request)
+        
+        # Sending response
+        response_data = self.prepare_response(request)
         sock.send(response_data)
         logger.info('Response sent to the client')
+
+    def prepare_response(self, request):
+        response_request = pkipplib.IPPRequest(operation_id=pkipplib.CUPS_GET_DEFAULT)
+        response_request.operation["attributes-charset"] = ("charset", "utf-8")
+        response_request.operation["attributes-natural-language"] = ("naturalLanguage", "en-us")
+        return response_request.dump()
 
     def get_server(self, host, port):
         connection = (host, port)
@@ -74,5 +71,5 @@ class PrintServer(object):
 
 if __name__ == "__main__":
     ps = PrintServer()
-    print_server = ps.get_server("localhost", 9100)
+    print_server = ps.get_server("0.0.0.0", 9100)
     print_server.serve_forever()
